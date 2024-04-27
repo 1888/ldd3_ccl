1ec2945a432ac040c637e94f4cebd093076b6000
1888 <cl_chen@163.com>
2024-04-27 23:24:23 +0800

    Chapter6: add jit folder

--- chapter6/jit.c
+++ chapter6/jit.c
@@ -7,7 +7,10 @@
 #include <linux/timer.h>
 
 /* JIT stands for "Just In Time" */
+#define JIT "jit"
 #define JIT_CURRENT_TIME "currentime"
+
+static struct proc_dir_entry *jit_folder;
 
 /*
  * This file, on the other hand, returns the current time forever
@@ -19,16 +22,16 @@ int jit_currentime_show(struct seq_file *m, void *v)
 	unsigned long j1;
 	u64 j2;
 
-/*
- * The counter is initialized to 0 at system boot, so it represents the
- * number of clock ticks since last boot. The counter is a 64-bit
- * variable (even on 32-bit architectures) and is called jiffies_64.
- * However, driver writers normally access the jiffies variable, an
- * unsigned long that is the same as either jiffies_64 or its least
- * significant bits. Using jififes is usually preferred bacause it is
- * faster, and access to the 64-bit jiffies_64 value are not necessary
- * atomic on 32-bit architectures).
- */
+	/*
+	 * The counter is initialized to 0 at system boot, so it represents the
+	 * number of clock ticks since last boot. The counter is a 64-bit
+	 * variable (even on 32-bit architectures) and is called jiffies_64.
+	 * However, driver writers normally access the jiffies variable, an
+	 * unsigned long that is the same as either jiffies_64 or its least
+	 * significant bits. Using jififes is usually preferred bacause it is
+	 * faster, and access to the 64-bit jiffies_64 value are not necessary
+	 * atomic on 32-bit architectures).
+	 */
 	j1 = jiffies;
 	j2 = get_jiffies_64();
 	do_gettimeofday(&tv1);
@@ -56,14 +59,35 @@ static const struct file_operations jit_currentime_fops = {
 
 int __init jit_init(void)
 {
-	proc_create_data(JIT_CURRENT_TIME, 0, NULL, &jit_currentime_fops, NULL);
+	int ret;
+	struct proc_dir_entry *current_time;
+	jit_folder = proc_mkdir(JIT, NULL);
+	if (!jit_folder) {
+		pr_err("Failed to create /proc/jit folder\n");
+		return -ENOMEM;
+	}
 
+	current_time = proc_create_data(JIT_CURRENT_TIME, 0666, jit_folder, &jit_currentime_fops, NULL);
+	if(!current_time) {
+		pr_err("Failed to create /proc/jit/current_time\n");
+		ret = -ENOMEM;
+		goto remove_jit;
+	}
+
+	pr_info("insmod /proc/jit\n");
 	return 0; /* success */
+
+remove_jit:
+	remove_proc_entry(JIT, NULL);
+	return ret;
 }
 
 void __exit jit_cleanup(void)
 {
-	remove_proc_entry(JIT_CURRENT_TIME, NULL);
+	pr_info("remove /proc/jit/current_time\n");
+	remove_proc_entry(JIT_CURRENT_TIME, jit_folder);
+	pr_info("remove /proc/jit\n");
+	remove_proc_entry(JIT, NULL);
 }
 
 module_init(jit_init);
